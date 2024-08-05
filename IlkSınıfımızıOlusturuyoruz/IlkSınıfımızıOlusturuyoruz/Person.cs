7f725ed8cc3e6175ce4c1ccb63fea4187a9e3823
Sefa Saraç <srcsfysf@gmail.com>
2024-08-05 22:53:54 +0300

    Add files via upload

---
+++ IlkSınıfımızıOlusturuyoruz/IlkSınıfımızıOlusturuyoruz/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sınıf
+{
+    public class Person
+    {
+        // Properties
+        public string ad;
+        public string soyad;
+        public DateTime dogumTarihi;
+
+        public string Ad
+        {
+            get
+            {
+                return ad;
+            }
+            set
+            {
+                ad = value;
+            }
+        }
+        public string Soyad
+        {
+            get
+            {
+                return soyad;
+            }
+            set
+            {
+                soyad = value;
+            }
+        }
+        public DateTime DogumTarihi
+        {
+            get
+            {
+                return dogumTarihi;
+            }
+            set
+            {
+                dogumTarihi = value;
+            }
+        }
+        public void BilgileriYazdır()     // Display method
+        {
+            Console.WriteLine($"Ad: {ad}");
+            Console.WriteLine($"Soyad: {soyad}");
+            Console.WriteLine($"Doğum tarihi: {dogumTarihi.ToShortDateString()}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ IlkSınıfımızıOlusturuyoruz/IlkSınıfımızıOlusturuyoruz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Sınıf
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Creating student objects
+            Person ogrenci1 = new Person();
+            ogrenci1.Ad = "Ali";
+            ogrenci1.Soyad = "Yılmaz";
+            ogrenci1.DogumTarihi = new DateTime(1994, 5, 10);
+            Person ogrenci2 = new Person();
+            ogrenci2.Ad = "Ayse";
+            ogrenci2.Soyad = "Kara";
+            ogrenci2.DogumTarihi = new DateTime(1998, 6, 11);
+
+            // Creating teacher objects
+            Person ogretmen1 = new Person();
+            ogretmen1.Ad = "Mehmet";
+            ogretmen1.Soyad = "Öztürk";
+            ogretmen1.DogumTarihi = new DateTime(1978, 4, 1);
+            Person ogretmen2 = new Person();
+            ogretmen2.Ad = "Yeliz";
+            ogretmen2.Soyad = "Işık";
+            ogretmen2.DogumTarihi = new DateTime(1984, 2, 14);
+
+            // Information is display
+            Console.WriteLine("Öğrenci Bilgileri:");
+            ogrenci1.BilgileriYazdır();
+            ogrenci2.BilgileriYazdır();
+
+            Console.WriteLine("Öğretmen Bilgileri:");
+            ogretmen1.BilgileriYazdır();
+            ogretmen2.BilgileriYazdır();
+        }
+    }
+}
